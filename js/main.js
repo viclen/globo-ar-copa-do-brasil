371cbcc9389129aa0c7ce484996d46899505cbf7
viclen <victor.lengler@gmail.com>
2021-02-13 10:46:33 -0300

    movement speed

--- js/main.js
+++ js/main.js
@@ -130,9 +130,9 @@ AFRAME.registerComponent('scene-objects', {
             const deltaTime = (now - lastMotion.time) / 1000;
 
             const newPosition = {
-                x: position.x - lastMotion.x * deltaTime,
-                y: position.y - lastMotion.y * deltaTime,
-                z: position.z - lastMotion.z * deltaTime
+                x: position.x + lastMotion.x,
+                y: position.y + lastMotion.y,
+                z: position.z + lastMotion.z
             };
 
             this.el.object3D.position.set(newPosition.x, newPosition.y, newPosition.z);
