249d0895f4378a669240917484c9fd1e7aace9e1
viclen <victor.lengler@gmail.com>
2021-02-18 11:58:10 -0300

    camera test

--- js/front-camera.js
+++ js/front-camera.js
@@ -1,52 +1,28 @@
-// const video = document.getElementById('video');
-const button = document.getElementById('button');
-const select = document.getElementById('cameraRotation');
-let currentStream;
-
-function stopMediaTracks(stream) {
-    stream.getTracks().forEach(track => {
-        track.stop();
-    });
+const constraints = { video: { facingMode: "user" }, audio: false };
+// Define constants
+const cameraView = document.querySelector("#camera--view"),
+    cameraOutput = document.querySelector("#camera--output"),
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger")
+// Access the device camera and stream to cameraView
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function (stream) {
+            track = stream.getTracks()[0];
+            cameraView.srcObject = stream;
+        })
+        .catch(function (error) {
+            console.error("Oops. Something is broken.", error);
+        });
 }
-
-function gotDevices(mediaDevices) {
-    select.innerHTML = '';
-    let html = "";
-    mediaDevices.forEach(mediaDevice => {
-        if (mediaDevice.kind === 'videoinput') {
-            html += mediaDevice.deviceId + " - " + mediaDevice.label + "<br />";
-        }
-    });
-    select.innerHTML = html;
-}
-
-// button.addEventListener('click', event => {
-//     if (typeof currentStream !== 'undefined') {
-//         stopMediaTracks(currentStream);
-//     }
-//     const videoConstraints = {};
-//     if (select.value === '') {
-//         videoConstraints.facingMode = 'environment';
-//     } else {
-//         videoConstraints.deviceId = { exact: select.value };
-//     }
-//     const constraints = {
-//         video: videoConstraints,
-//         audio: false
-//     };
-//     navigator.mediaDevices
-//         .getUserMedia(constraints)
-//         .then(stream => {
-//             currentStream = stream;
-//             video.srcObject = stream;
-//             return navigator.mediaDevices.enumerateDevices();
-//         })
-//         .then(gotDevices)
-//         .catch(error => {
-//             console.error(error);
-//         });
-// });
-
-navigator.mediaDevices.getUserMedia({ video: true }).then(() => {
-    navigator.mediaDevices.enumerateDevices().then(gotDevices);
-})
+// Take a picture when cameraTrigger is tapped
+cameraTrigger.onclick = function () {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add("taken");
+};
+// Start the video stream when the window loads
+window.addEventListener("load", cameraStart, false);
