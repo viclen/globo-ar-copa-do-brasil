693f9c205271641094360acb28cb514cb0f56943
viclen <victor.lengler@gmail.com>
2021-02-23 19:51:55 -0300

    permission

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+document.addEventListener("load", function () {
+    navigator.permissions.query({ name: 'gyroscope' }).then(function (result) {
+        if (result.state == "denied") {
+            alert("Sem permissão de movimento.");
+        }
+    });
+
+    (function requestDeviceMotion(callback) {
+        if (window.DeviceMotionEvent == null) {
+            alert("Sem permissão de movimento.");
+        } else if (DeviceMotionEvent.requestPermission) {
+            DeviceMotionEvent.requestPermission().then(function (state) {
+                if (state == "denied") {
+                    alert("Sem permissão de movimento.");
+                }
+            }, function (err) {
+                alert("Sem permissão de movimento.");
+            });
+        }
+    })();
+});
+
 window.mobileCheck = function () {
     let check = false;
     (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
@@ -28,7 +50,8 @@ let velocity = { x: 0, y: 0, z: 0 };
 let motions = {
     x: [],
     y: [],
-    z: []
+    z: [],
+    time: new Date().getTime()
 };
 
 let canAdd = true;
@@ -85,13 +108,7 @@ AFRAME.registerComponent('ar-scene', {
 
             clickToStart.remove();
 
-            window.addEventListener("devicemotion", onMoveDevice);
-
-            document.addEventListener("click", (event) => {
-                if (event.target.classList.contains("fixed-front")) {
-                    createParticles();
-                }
-            });
+            // window.addEventListener("devicemotion", onMoveDevice);
 
             document.getElementById("carregando").style.display = "";
             document.querySelector(".fixed-front").remove();
@@ -110,20 +127,14 @@ AFRAME.registerComponent('3dmodel', {
 function onMoveDevice(event) {
     let acl = event.acceleration;
 
-    if (acl.x !== null) {
+    if (acl.x !== null && motions.x.length < 5) {
         const now = new Date().getTime();
-        const deltaTime = (now - acceleration.time) / 1000;
+        const deltaTime = (now - motions.time) / 1000;
 
-        acceleration = {
-            x: Math.round(acl.x.withTolerance(0)) * deltaTime,
-            y: Math.round(acl.y.withTolerance(0.2) - 9.81) * deltaTime,
-            z: Math.round(acl.z.withTolerance(0)) * deltaTime,
-            time: now
-        }
-
-        velocity.x += acceleration.x;
-        velocity.y += acceleration.y;
-        velocity.z += acceleration.z;
+        motions.x.push(acl.x * 0.5 * deltaTime ** 2);
+        motions.y.push(acl.y * 0.5 * deltaTime ** 2);
+        motions.z.push(acl.z * 0.5 * deltaTime ** 2);
+        motions.time = now;
     }
 }
 
@@ -150,70 +161,45 @@ function movementThreshold(acl) {
     }
 }
 
-function createParticles() {
-    if (!canAdd) return;
+// AFRAME.registerComponent('scene-objects', {
+//     tick: (function () {
+//         const position = new THREE.Vector3();
 
-    canAdd = false;
+//         return function () {
+//             if (!position || isNaN(position.x) || isNaN(position.y) || isNaN(position.z)) {
+//                 return;
+//             }
 
-    // document.getElementById("particles").innerHTML = `
-    //     <a-entity sprite-particles="
-    //         spawnRate: 30;
-    //         texture: url(img/star.png);
-    //         lifeTime: 1..5;
-    //         radialVelocity: 2..6;
-    //         radialAcceleration: -1;
-    //         opacity: 1,0;
-    //         color: white;
-    //         scale: 4;
-    //         spawnType: burst;
-    //     "></a-entity>
-    // `;
+//             const move = {
+//                 x: motions.x.shift(),
+//                 y: motions.y.shift(),
+//                 z: motions.z.shift(),
+//             }
 
-    setTimeout(() => {
-        document.getElementById("particles").innerHTML = "";
-        canAdd = true;
-    }, 2000);
-}
+//             const newPosition = {
+//                 x: position.x + move.x,
+//                 y: position.y,
+//                 z: position.z + move.z
+//             };
 
-AFRAME.registerComponent('scene-objects', {
-    tick: (function () {
-        const position = new THREE.Vector3();
-        // const quaternion = new THREE.Quaternion();
+//             if (cameraRig) {
+//                 cameraRig.setAttribute("position", newPosition);
+//             }
 
-        return function () {
-            // this.el.object3D.getWorldPosition(position);
-            // this.el.object3D.getWorldQuaternion(quaternion);
+//             document.getElementById("cameraPosition").innerHTML = `
+//                 ${move.x}<br />
+//                 ${move.y}<br />
+//                 ${move.z}<br />
+//             `;
 
-            if (!position || isNaN(position.x) || isNaN(position.y) || isNaN(position.z)) {
-                return;
-            }
-
-            const now = new Date().getTime();
-            const deltaTime = (now - lastFrame) / 1000;
-            lastFrame = now;
-
-            // const newPosition = {
-            //     x: position.x - velocity.x * deltaTime * 0,
-            //     y: position.y - velocity.y * deltaTime * 0,
-            //     z: position.z - velocity.z * deltaTime
-            // };
-
-            // this.el.object3D.position.set(newPosition.x, newPosition.y, newPosition.z);
-
-            // document.getElementById("cameraPosition").innerHTML = `
-            //     ${velocity.x},
-            //     ${velocity.y},
-            //     ${velocity.z}
-            // `;
-
-            // document.getElementById("cameraRotation").innerHTML = `
-            //     ${Math.round(quaternion.x * 180)},
-            //     ${Math.round(quaternion.y * 180)},
-            //     ${Math.round(quaternion.z * 180)}
-            // `;
-        };
-    })()
-});
+//             document.getElementById("cameraRotation").innerHTML = `
+//                 ${newPosition.x}<br />
+//                 ${newPosition.y}<br />
+//                 ${newPosition.z}<br />
+//             `;
+//         };
+//     })()
+// });
 
 AFRAME.registerComponent('rotation-reader', {
     init: function () {
@@ -226,20 +212,9 @@ AFRAME.registerComponent('rotation-reader', {
                 const rigRotation = cameraRig.getAttribute("rotation");
 
                 rigRotation.x = -rotation.x * 2;
+                // rigRotation.y = 180 + rotation.y;
 
                 cameraRig.setAttribute("rotation", rigRotation);
-
-                document.getElementById("cameraPosition").innerHTML = `
-                    ${Math.round(rigRotation.x)},
-                    ${Math.round(rigRotation.y)},
-                    ${Math.round(rigRotation.z)}
-                `;
-
-                document.getElementById("cameraRotation").innerHTML = `
-                    ${Math.round(rotation.x)},
-                    ${Math.round(rotation.y)},
-                    ${Math.round(rotation.z)}
-                `;
             }
         };
     })()
@@ -259,6 +234,7 @@ function zoomIn() {
     }
 
     object3d.setAttribute("animation", `property: scale; to: ${newScale} ${newScale} ${newScale}; dur: 500; easing: linear; loop: false`);
+    document.getElementById("particles").setAttribute("sprite-particles", { scale: (newScale * 0.4) + "" });
 }
 
 function zoomOut() {
@@ -273,6 +249,16 @@ function zoomOut() {
     }
 
     object3d.setAttribute("animation", `property: scale; to: ${newScale} ${newScale} ${newScale}; dur: 500; easing: linear; loop: false`);
+    document.getElementById("particles").setAttribute("sprite-particles", { scale: (newScale * 0.4) + "" });
 }
 
-THREE.BufferGeometry.prototype.addAttribute = THREE.BufferGeometry.prototype.setAttribute;
+function enableParticles() {
+    const el = document.getElementById("enable-particles");
+    if (el.classList.contains("active")) {
+        el.classList.remove("active");
+        document.getElementById("particles").setAttribute("sprite-particles", { enable: false });
+    } else {
+        document.getElementById("particles").setAttribute("sprite-particles", { enable: true });
+        el.classList.add("active");
+    }
+}
